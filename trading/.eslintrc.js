6b196358ec657bb1a2551cd1eaaffb1742b00ab6
백운범 <baek12201@gmail.com>
2021-09-16 13:24:29 +0900

    chore: alias 설정
    - alias란?
      디렉토리 구조가 복잡해질 수록 상대경로로 파일 import 구문이 복잡해집니다.
      alias를 활용하면 절대경로로 접근하듯이 import 구문이 심플해집니다.
    
    - tsconfig.paths.json을 수정하면 alias 수정과 추가가 가능합니다!

--- trading/.eslintrc.js
+++ trading/.eslintrc.js
@@ -34,9 +34,7 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+      typescript: {},
     },
   },
 };
--- trading/src/App.tsx
+++ trading/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Dashboard from "./components/layout/Dashboard";
+import React from 'react';
+import './App.css';
+import Dashboard from '@components/layout/Dashboard';
 
 function App() {
   return (
