1029c5f773d10faf8822811d7edd3e12ed4e4a13
mong <momomingzhi@gmail.com>
2021-09-21 00:13:01 +0900

    fix: search component  수정

--- trading/src/routes/normalRoute/CoinMarket.tsx
+++ trading/src/routes/normalRoute/CoinMarket.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Grid } from '@material-ui/core';
 import ReactVirtualizedTable from '@components/common/ReactVirtualizedTable';
 import Box from '@mui/material/Box';
 import { TimeLine } from '@containers/CoinMarket/TimeLineContainer';
+import { CommonInputContainer } from '@containers/common/InputContainer';
 
 const CoinMarket = () => {
+  const [search, setsearch] = useState('');
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setsearch(e.target.value);
+  };
+  const handleKeyPress = () => {
+    console.log('press key: ', search);
+  };
   return (
     <Container>
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Box sx={{ width: 300, margin: '0rem 30rem 0rem 0rem' }}>
             <h1>Coin market</h1>
+          </Box>
+          <Box component="div" sx={{ display: 'flex', flexDirection: 'row-reverse', margin: '0rem 0rem 1rem 0rem' }}>
+            <CommonInputContainer placeholder="search" onChange={handleInputChange} onKeyPress={handleKeyPress} />
           </Box>
 
           <ReactVirtualizedTable />
