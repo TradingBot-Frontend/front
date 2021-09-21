eea4137998bb5394c99b4cdeef87afd1b7d7076d
mong <momomingzhi@gmail.com>
2021-09-21 19:44:53 +0900

    feat: coin market redux-saga 추가

--- trading/src/redux/sagas/index.ts
+++ trading/src/redux/sagas/index.ts
@@ -2,11 +2,12 @@ import { all, fork } from 'redux-saga/effects';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import authSaga from './authSaga';
+import coinSaga from './coinSaga';
 
 // dotenv.config();
 // axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
   // 여기에 saga 추가하면 됩니다!
-  yield all([fork(authSaga)]);
+  yield all([fork(authSaga), fork(coinSaga)]);
 }
