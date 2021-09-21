3e9bcac1f4aa575ba71c4a032a4de98b710013e1
mong <momomingzhi@gmail.com>
2021-09-22 00:37:53 +0900

    feat: private setting component 추가

--- trading/src/routes/normalRoute/Main.tsx
+++ trading/src/routes/normalRoute/Main.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { Box } from '@material-ui/core';
+import PrivateSetting from '@containers/Dashboard/privateSetting';
+import styled from 'styled-components';
 
+const Buttons = styled(Button)`
+  background-color: #231381;
+`;
 const CoinMarket = () => {
-  return <div>Dashboard Main Page</div>;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('클릭!');
+  };
+  return (
+    <>
+      <div>Dashboard Main Page</div>
+      <Button onClick={handleOpen}>private setting</Button>
+      <Modal open={open} onClose={handleClose}>
+        <>
+          <PrivateSetting />
+        </>
+      </Modal>
+    </>
+  );
 };
 export default CoinMarket;
