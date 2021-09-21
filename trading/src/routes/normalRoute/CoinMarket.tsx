67066f3e49e9f1eff3f4697448c1abc75c0ec875
mong <momomingzhi@gmail.com>
2021-09-21 12:40:58 +0900

    feat: coin market > 검색 기능 추가

--- trading/src/routes/normalRoute/CoinMarket.tsx
+++ trading/src/routes/normalRoute/CoinMarket.tsx
@@ -1,17 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Grid } from '@material-ui/core';
-import ReactVirtualizedTable from '@components/common/ReactVirtualizedTable';
+import { ReactVirtualizedTable } from '@components/common/ReactVirtualizedTable';
 import Box from '@mui/material/Box';
 import { TimeLine } from '@containers/CoinMarket/TimeLineContainer';
 import { CommonInputContainer } from '@containers/common/InputContainer';
 
+interface Data {
+  name: string;
+  currentPrice: string;
+  rateOfChange: string;
+  money: string;
+  id: number;
+}
+interface stateProps {
+  coinContent: Data[];
+  filterCoinContent: Data[];
+  keyword: string;
+}
+type Sample = [string, string, string, string];
+
+const sample: readonly Sample[] = [
+  ['비트코인', '55,000,000', '+2.5%', '151.445'],
+  ['이클', '55,000,000', '+2.5%', '151.445'],
+  ['도지코인', '55,000,000', '+2.5%', '151.445'],
+  ['비트토렌트', '55,000,000', '+2.5%', '151.445'],
+  ['페이코인', '55,000,000', '+2.5%', '151.445'],
+];
+
+function createData(id: number, name: string, currentPrice: string, rateOfChange: string, money: string): Data {
+  return { id, name, currentPrice, rateOfChange, money };
+}
+
+const rows: Data[] = [];
+
+for (let i = 0; i < 200; i += 1) {
+  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+  rows.push(createData(i, ...randomSelection));
+}
+
 const CoinMarket = () => {
   const [search, setsearch] = useState('');
+  const [states, setStates] = useState<any>({
+    coinContent: [],
+    filterCoinContent: [],
+    keyword: '',
+  });
+  const { coinContent, filterCoinContent, keyword } = states;
+  useEffect(() => {
+    setStates({
+      ...states,
+      coinContent: rows,
+      filterCoinContent: rows,
+    });
+  }, []);
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setsearch(e.target.value);
+    // setsearch(e.target.value);
+    setStates({
+      ...states,
+      keyword: e.target.value,
+    });
   };
   const handleKeyPress = () => {
     console.log('press key: ', search);
+    if (keyword) {
+      setStates({
+        ...states,
+        filterCoinContent: coinContent.filter((data: Data) => {
+          return data.name.includes(keyword);
+        }),
+      });
+    } else {
+      setStates({
+        ...states,
+        filterCoinContent: coinContent,
+      });
+    }
   };
   return (
     <Container>
@@ -24,7 +87,7 @@ const CoinMarket = () => {
             <CommonInputContainer placeholder="search" onChange={handleInputChange} onKeyPress={handleKeyPress} />
           </Box>
 
-          <ReactVirtualizedTable />
+          <ReactVirtualizedTable rows={filterCoinContent} />
         </Grid>
         <Grid item xs={6}>
           <Box sx={{ width: 300, margin: '0rem 30rem 0rem 0rem' }}>
