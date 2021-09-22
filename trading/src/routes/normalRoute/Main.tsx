e3d3051ce0b9867400798467d6fd79acf8f42e8e
DESKTOP-UI5I6K2\jang <myeongji_jang@tmax.co.kr>
2021-09-22 18:35:21 +0900

    feat: dashboard 기본 구성 추가

--- trading/src/routes/normalRoute/Main.tsx
+++ trading/src/routes/normalRoute/Main.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import { Box } from '@material-ui/core';
+import { Box, Divider } from '@material-ui/core';
 import PrivateSetting from '@containers/Dashboard/privateSetting';
 import styled from 'styled-components';
+import { Container, Grid } from '@material-ui/core';
 
-const Buttons = styled(Button)`
-  background-color: #231381;
+import { MybotBoard } from '@containers/Dashboard/MybotBoard';
+import DsbCoinList from '@containers/Dashboard/DsbCoinList';
+
+const MainWapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 10rem;
+  .buttons {
+    display: flex;
+    flex-direction: row-reverse;
+  }
 `;
-const CoinMarket = () => {
+const Main = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -16,15 +27,27 @@ const CoinMarket = () => {
     console.log('클릭!');
   };
   return (
-    <>
-      <div>Dashboard Main Page</div>
-      <Button onClick={handleOpen}>private setting</Button>
-      <Modal open={open} onClose={handleClose}>
-        <>
+    <MainWapper>
+      <div className="buttons">
+        <Button style={{ background: '#3072eb', color: 'white' }} onClick={handleOpen}>
+          private setting
+        </Button>
+      </div>
+      <Container>
+        <Grid item xs={6}>
+          <MybotBoard />
+        </Grid>
+        <Divider />
+        <Grid container justify="center">
+          <Box>
+            <DsbCoinList />
+          </Box>
+        </Grid>
+        <Modal open={open} onClose={handleClose}>
           <PrivateSetting handleClose={handleClose} />
-        </>
-      </Modal>
-    </>
+        </Modal>
+      </Container>
+    </MainWapper>
   );
 };
-export default CoinMarket;
+export default Main;
