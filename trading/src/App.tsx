69cf8797a7df71a32a968e21f39b3392833aaf8b
백운범 <woonbeom_baek@tmax.co.kr>
2021-09-24 17:04:04 +0900

    feat: 새로고침시, token 유효할 때 자동 로그인 기능 구현중

--- trading/src/App.tsx
+++ trading/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from '@redux/store';
 import './App.css';
 import MainRouter from '@routes/MainRouter';
+import login from '@utils/login';
+import { useHistory } from 'react-router';
 
 function App(): JSX.Element {
+  const h = useHistory();
+
+  useEffect(() => {
+    if (login()) {
+      h.push('/main');
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
