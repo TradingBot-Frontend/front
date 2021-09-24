809b6d794a6a8b24de75ecd59d98fe3a5be6babb
백운범 <baek12201@gmail.com>
2021-09-24 09:18:14 +0900

    feat: BotCard 컴포넌트 title, profit 인자를 받음
    - On, Off Typography 추가

--- trading/src/components/TradingBot/BotCard.tsx
+++ trading/src/components/TradingBot/BotCard.tsx
@@ -7,7 +7,12 @@ import { styled } from '@mui/material/styles';
 import ApexChart from 'react-apexcharts';
 import sb from '../../assets/images/sb.png';
 
-export default function BotCard(): JSX.Element {
+interface BotCardProps {
+  title: string;
+  profit: string;
+}
+
+export default function BotCard({ title, profit }: BotCardProps): JSX.Element {
   const [chart, setChart] = useState({
     series: [
       {
@@ -17,26 +22,42 @@ export default function BotCard(): JSX.Element {
     ],
     options: {
       chart: {
-        id: 'area-chart',
+        id: 'tradingbot-chart',
         zoom: {
           enabled: false,
+        },
+        toolbar: {
+          show: false,
+          tools: {
+            download: false,
+          },
         },
       },
       dataLabels: {
         enabled: false,
       },
+      yaxis: {
+        labels: {
+          show: false,
+        },
+      },
+      xaxis: {
+        labels: {
+          show: false,
+        },
+      },
       //   stroke: {
       //     curve: 'straight',
       //   },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shadeIntensity: 1,
-          opacityFrom: 0.7,
-          opacityTo: 0.9,
-          stops: [0, 90, 100],
-        },
-      },
+      // fill: {
+      //   type: 'gradient',
+      //   gradient: {
+      //     shadeIntensity: 1,
+      //     opacityFrom: 0.7,
+      //     opacityTo: 0.9,
+      //     stops: [0, 90, 100],
+      //   },
+      // },
       //   xaxis: {
       //     categories: [
       //       '01 Jan',
@@ -53,19 +74,22 @@ export default function BotCard(): JSX.Element {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Stack spacing={2}>
+        <Stack spacing={1}>
           <Stack spacing={2} direction="row">
             <img src={sb} alt="coin" width="70" height="70" />
             <Stack spacing={2}>
-              <Typography>변동성 돌파 전략1</Typography>
-              <Typography>수익률 + 25.4%</Typography>
+              <Typography>{title}</Typography>
+              <Typography>{profit}</Typography>
             </Stack>
+            <Typography variant="h2" gutterBottom>
+              On
+            </Typography>
           </Stack>
           <ApexChart
             options={chart.options}
             series={chart.series}
             type="area"
-            height={200}
+            height={150}
           />
         </Stack>
       </CardContent>
--- trading/src/routes/normalRoute/TradingBot.tsx
+++ trading/src/routes/normalRoute/TradingBot.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import ContentWrapper from '@components/common/ContentWrapper';
 import BotCard from '@components/TradingBot/BotCard';
 import Grid from '@mui/material/Grid';
@@ -10,25 +8,25 @@ const TradingBot = (): JSX.Element => {
     <ContentWrapper title="TradingBot" addButton={<>add</>}>
       <Grid container spacing={2}>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
-          <BotCard />
+          <BotCard title="변동성 돌파 전략1" profit="수익률 +25.4%" />
         </Grid>
       </Grid>
     </ContentWrapper>
