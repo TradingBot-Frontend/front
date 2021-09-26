e8f5efe6b10c86a5059045a99d1adf64f647bebf
mong <momomingzhi@gmail.com>
2021-09-27 01:39:07 +0900

    feat: websocket saga 추가

--- trading/src/redux/reducers/index.ts
+++ trading/src/redux/reducers/index.ts
@@ -3,13 +3,15 @@ import { combineReducers, Reducer } from 'redux';
 import { History } from 'history';
 import authReducer from './authReducer';
 import portfolioReducer from './portfolioReducer';
+import livePrices from './LivePrices';
 
 const createRootReducer = (history: History): Reducer =>
   // 여기에 reducer 추가하면 됩니다!
   combineReducers({
     router: connectRouter(history),
     auth: authReducer,
-    portfolio: portfolioReducer
+    portfolio: portfolioReducer,
+    live: livePrices,
   });
 
 export type RootState = ReturnType<ReturnType<typeof createRootReducer>>;
--- trading/src/redux/sagas/index.ts
+++ trading/src/redux/sagas/index.ts
@@ -4,11 +4,17 @@ import axios from 'axios';
 import authSaga from './authSaga';
 import coinSaga from './coinSaga';
 import portfolioSaga from './portfolioSaga';
+import { watchLivePricesSaga } from './LivePricesSaga';
 
 // dotenv.config();
 // axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
   // 여기에 saga 추가하면 됩니다!
-  yield all([fork(authSaga), fork(coinSaga), fork(portfolioSaga)]);
+  yield all([
+    fork(authSaga),
+    fork(coinSaga),
+    fork(portfolioSaga),
+    fork(watchLivePricesSaga),
+  ]);
 }
