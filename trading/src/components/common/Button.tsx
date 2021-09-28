7f69adfbeb4f765477806efdcdf07c950e80d681
백운범 <woonbeom_baek@tmax.co.kr>
2021-09-28 10:37:16 +0900

    fix: CommonButton에 color 지정할 수 있게 옵션 추가

--- trading/src/components/common/Button.tsx
+++ trading/src/components/common/Button.tsx
@@ -2,10 +2,16 @@ import Button from '@material-ui/core/Button';
 import React from 'react';
 import { ButtonProps } from '@containers/common/ButtonContainer';
 
-export const CommonButton: React.FC<ButtonProps> = ({ title, onClick }) => {
+export const CommonButton: React.FC<ButtonProps> = ({
+  title,
+  color,
+  onClick,
+}) => {
   return (
     <div>
-      <Button onClick={onClick}>{title}</Button>
+      <Button style={{ color }} onClick={onClick}>
+        {title}
+      </Button>
     </div>
   );
 };
