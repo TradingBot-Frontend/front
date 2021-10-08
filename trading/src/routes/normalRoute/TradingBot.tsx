53c56b03a6bf704fe6ca63804b2b212e595eb38a
백운범 <baek12201@gmail.com>
2021-10-08 15:32:58 +0900

    fix: 트레이딩봇이 없을 때 카드 렌더링 에러 수정

--- trading/src/routes/normalRoute/TradingBot.tsx
+++ trading/src/routes/normalRoute/TradingBot.tsx
@@ -17,20 +17,6 @@ const TradingBot = (): JSX.Element => {
   const dispatch = useDispatch();
   const bots = useSelector((state: RootState) => state.bot.bots);
   const isLoading = useSelector((state: RootState) => state.bot.isLoading);
-  const cards = [
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-    { title: '변동성 돌파 전략1', profit: '수익률 +25.4%' },
-  ];
-
   useEffect(() => {
     dispatch(getBotsActions.request());
   }, [dispatch]);
@@ -39,28 +25,13 @@ const TradingBot = (): JSX.Element => {
     <>
       <ContentWrapper title="TradingBot" addButton handleOpen={handleOpen}>
         <Grid container spacing={1}>
-          {/* {cards.map(({ title, profit }) => (
-            <Grid key={title} item xl={3} lg={4} md={6} sm={12}>
-              <BotSkeleton />
-            </Grid>
-          ))} */}
-          {/* {cards.map(({ title, profit }) => (
-            <Grid key={title} item xl={3} lg={4} md={6} sm={12}>
-              <BotCard
-                title={title}
-                profit={profit}
-                botInfo={{}}
-                width={380}
-                // isLoading={isLoading}
-                isLoading={false}
-              />
-            </Grid>
-          ))} */}
-          {bots.map((bot: Bot) => (
-            <Grid key={bot.id} item xl={3} lg={4} md={6} sm={12}>
-              <BotCard botInfo={bot} width={380} isLoading={isLoading} />
-            </Grid>
-          ))}
+          {bots.length > 0
+            ? bots.map((bot: Bot) => (
+                <Grid key={bot.id} item xl={3} lg={4} md={6} sm={12}>
+                  <BotCard botInfo={bot} width={380} isLoading={isLoading} />
+                </Grid>
+              ))
+            : null}
         </Grid>
       </ContentWrapper>
       <Dialog open={open} onClose={handleClose}>
