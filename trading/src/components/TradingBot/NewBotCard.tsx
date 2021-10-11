483bd5bff613f8b2d34b84c4ac2539a5f0ab26d4
백운범 <baek12201@gmail.com>
2021-10-11 14:55:45 +0900

    design: 트레이딩봇 ON/OFF 여부 chip 컴포넌트로 변경

--- trading/src/components/TradingBot/NewBotCard.tsx
+++ trading/src/components/TradingBot/NewBotCard.tsx
@@ -10,18 +10,31 @@ import TradingBotAdd from '@containers/TradingBot/TradingAddContainer';
 import { Bot } from '@redux/reducers/botReducer';
 import icons from '@assets/images';
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 
 interface NewBotCardProps {
   botInfo: Bot;
-  // width?: number;
   isLoading: boolean;
+}
+
+interface OnOffChipProps {
+  on: boolean;
+}
+
+function OnOffChip({ on }: OnOffChipProps): JSX.Element {
+  return (
+    <Chip
+      label={on ? 'ON' : 'OFF'}
+      color={on ? 'success' : 'error'}
+      sx={{ cursor: 'pointer' }}
+    />
+  );
 }
 
 export default function NewBotCard({
   botInfo,
-  // width = 380,
   isLoading = false,
-}: NewBotCardProps) {
+}: NewBotCardProps): JSX.Element {
   const [icon, setIcon] = useState('');
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -31,52 +44,56 @@ export default function NewBotCard({
     setIcon(icons[botInfo.coinName]);
   }, []);
   return (
-    <Card sx={{ cursor: 'pointer', width: '14rem' }} onClick={handleOpen}>
-      <CardContent>
-        <Stack spacing={1} sx={{}}>
-          <Box sx={flexCenter}>
-            {isLoading ? (
-              <Skeleton variant="circular" width={70} height={70}>
-                <Avatar />
-              </Skeleton>
-            ) : (
-              <img src={icon} alt="coin" width="70" height="70" />
-            )}
-          </Box>
-          <Box sx={flexCenter}>
-            {isLoading ? (
-              <Skeleton width="100%">
-                <Typography>##########################</Typography>
-              </Skeleton>
-            ) : (
-              <Typography sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
-                {botInfo.botName}
-              </Typography>
-            )}
-          </Box>
-          <Box sx={flexCenter}>
-            {isLoading ? (
-              <Skeleton width="100%">
-                <Typography>##########################</Typography>
-              </Skeleton>
-            ) : (
-              <Typography>수익률 {`${botInfo?.profit || 0}%`}</Typography>
-            )}
-          </Box>
-          <Box sx={flexCenter}>
-            {isLoading ? (
-              <Skeleton width="100%">
-                <Typography>##########################</Typography>
-              </Skeleton>
-            ) : (
-              <Typography>{botInfo.isActive ? 'ON' : 'OFF'}</Typography>
-            )}
-          </Box>
-        </Stack>
-      </CardContent>
+    <>
+      <Card sx={{ cursor: 'pointer', width: '14rem' }} onClick={handleOpen}>
+        <CardContent>
+          <Stack spacing={2}>
+            <Box sx={flexCenter}>
+              {isLoading ? (
+                <Skeleton variant="circular" width={70} height={70}>
+                  <Avatar />
+                </Skeleton>
+              ) : (
+                <img src={icon} alt="coin" width="70" height="70" />
+              )}
+            </Box>
+            <Box sx={flexCenter}>
+              {isLoading ? (
+                <Skeleton width="100%">
+                  <Typography>##########################</Typography>
+                </Skeleton>
+              ) : (
+                <Typography sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
+                  {botInfo.botName}
+                </Typography>
+              )}
+            </Box>
+            <Box sx={flexCenter}>
+              {isLoading ? (
+                <Skeleton width="100%">
+                  <Typography>##########################</Typography>
+                </Skeleton>
+              ) : (
+                <Typography>수익률 {`${botInfo?.profit || 0}%`}</Typography>
+              )}
+            </Box>
+            <Box sx={flexCenter}>
+              {isLoading ? (
+                <Skeleton width="100%">
+                  <Typography>##########################</Typography>
+                </Skeleton>
+              ) : (
+                <Typography>
+                  <OnOffChip on={botInfo.isActive} />
+                </Typography>
+              )}
+            </Box>
+          </Stack>
+        </CardContent>
+      </Card>
       <Dialog open={open} onClose={handleClose}>
         <TradingBotAdd botInfo={botInfo} handleClose={handleClose} />
       </Dialog>
-    </Card>
+    </>
   );
 }
